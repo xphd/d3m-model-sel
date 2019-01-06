396f9940d4d904f9bae1bb0b29cd455fb980eb36
alex <>
2019-01-05 21:51:41 -0500

    FlowChart works:

--- vue_frontend/src/components/PipelineView/PipelineView.vue
+++ vue_frontend/src/components/PipelineView/PipelineView.vue
@@ -23,91 +23,55 @@ export default {
     // requestPipeline defined in ZoomableScatter.vue
     responsePipeline(pipeline) {
       // this.pipeline = pipeline;
-      // this.init(pipeline);
-      this.init();
+      this.init(pipeline);
+      // this.init();
     }
   },
   methods: {
-    // init(pipeline) {}
-    init() {
-      this.nodes = [
-        {
-          id: "e1",
-          state: "",
-          value: {
-            label: "开始",
-            operator: "BashOperator"
+    init(pipeline) {
+      const elementId = { id: 0 };
+      // this.isPipelineShown = true;
+      // this.nodesPicked = [];
+      // this.nodes = [];
+      branch(pipeline, this.nodes, this.edges, elementId);
+
+      function branch(pipeline, nodes, edges, elementId) {
+        if (pipeline == null) {
+          return;
+        }
+        let steps = pipeline.steps;
+        for (let i = 0; i < steps.length; i++) {
+          let step = steps[i];
+          let name = step["primitive"]["primitive"]["name"];
+          let id = elementId.id;
+          let node = {
+            id: id,
+            state: "success",
+            value: {
+              label: name
+            }
+          };
+          let edge = {
+            from: id,
+            to: id + 1
+          };
+
+          if (i == steps.length - 1 && !step.pipeline) {
+            // node.next = [];
+          } else {
+            edges.push(edge);
           }
-        },
-        {
-          id: "e2",
-          state: "success",
-          value: {
-            label: "分支一",
-            operator: "BranchPythonMapOperator"
-          }
-        },
-        {
-          id: "e3",
-          state: "success",
-          value: {
-            label: "分支二",
-            operator: "BranchPythonMapOperator"
-          }
-        },
-        {
-          id: "e4",
-          state: "queued",
-          value: {
-            label: "节点3",
-            operator: "BashOperator"
-          }
-        },
-        {
-          id: "e5",
-          state: "failed",
-          value: {
-            label: "节点4",
-            operator: "BashOperator"
-          }
-        },
-        {
-          id: "e6",
-          state: "upstream_failed",
-          value: {
-            label: "结束",
-            operator: "JoinOperator"
+          nodes.push(node);
+
+          elementId.id += 1;
+
+          if (step.pipeline) {
+            branch(step.pipeline, nodes, edges, elementId);
+            // node.next.push(elementId.id);
+            edges.push(edge);
           }
         }
-      ];
-      this.edges = [
-        {
-          from: "e1",
-          to: "e2"
-        },
-        {
-          from: "e1",
-          to: "e3"
-        },
-        {
-          from: "e2",
-          to: "e4",
-          label: "条件1"
-        },
-        {
-          from: "e3",
-          to: "e5",
-          label: "条件2"
-        },
-        {
-          from: "e4",
-          to: "e6"
-        },
-        {
-          from: "e5",
-          to: "e6"
-        }
-      ];
+      }
     }
   }
 };
